9a2c09943245be225f43533bcfe9c340ca04cd62
Shin <Shin@DESKTOP-ARJCJ2G>
2025-01-13 13:51:28 +0800

    First Commit

---
+++ test-jsp-1/src/main/java/com/example/demo/TestJsp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJsp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJsp1Application.class, args);
+	}
+
+}
